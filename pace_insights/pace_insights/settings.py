22473629602b17a2e9b1dc0e0532c2b1b5f75049
Victor Liu <victor.liu@headforwards.com>
2015-03-16 07:58:28 +0000

    add static_root

--- pace_insights/pace_insights/settings.py
+++ pace_insights/pace_insights/settings.py
@@ -26,7 +26,7 @@ DEBUG = False
 
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.carfinancecompared.com',]
 
 
 # Application definition
@@ -90,7 +90,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
  
 # Redis
  
